c0672b44be86b20453e3e29904c04556d79752c3
Nico Danneberg <nico@danneberg.de>
2017-06-11 11:00:50 +0200

    some URL fixed to SSL

--- wp-wiki-tooltip.php
+++ wp-wiki-tooltip.php
@@ -2,11 +2,11 @@
 
 /*
 Plugin Name: WP Wiki Tooltip
-Plugin URI: http://n1da.net/specials/wp-wiki-tooltip/
+Plugin URI: https://n1da.net/specials/wp-wiki-tooltip/
 Description: Adds explaining tooltips querying their content from a <a href="https://www.mediawiki.org" target="_blank" rel="noopener noreferrer">MediaWiki</a> installation, e.g. <a href="https://www.wikipedia.org" target="_blank" rel="noopener noreferrer">Wikipedia.org</a>.
 Version: 1.7.2
 Author: nida78
-Author URI: http://n1da.net
+Author URI: https://n1da.net
 License: GPLv2 or later
 Text Domain: wp-wiki-tooltip
 */
